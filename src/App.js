b6da369a23f4f21b67cd0d714564e1c6958904fc
s1ckret <ivan.kovalenko@s1ckret.com>
2024-01-17 22:09:05 +0200

    feat: add simple EditorJs
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import React, { useState } from "react";
+import Editor from "./Editor";
+
+// Initial Data
+const INITIAL_DATA = {
+  time: new Date().getTime(),
+  blocks: [
+    {
+      type: "header",
+      data: {
+        text: "This is my awesome editor!",
+        level: 1,
+      },
+    },
+  ],
+};
+
 function App() {
+  const [data, setData] = useState(INITIAL_DATA);
   return (
-    <div></div>
+    <Editor data={data} onChange={setData} editorblock="editorjs-container" />
   );
 }
 

f0cf5131979bb12becc4ed92599aa9c02299a913
s1ckret <ivan.kovalenko@s1ckret.com>
2024-01-17 20:52:23 +0200

    feat: add basic blocknote editor
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { BlockNoteView, useBlockNote } from "@blocknote/react";
+import "@blocknote/react/style.css";
+
 function App() {
-  return (
-    <div></div>
-  );
+  // Creates a new editor instance.
+  const editor = useBlockNote({});
+
+  // Renders the editor instance using a React component.
+  return <BlockNoteView theme="light" editor={editor} />;
 }
 
 export default App;

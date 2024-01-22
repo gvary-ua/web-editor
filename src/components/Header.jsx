471f93bc24c8c29c4905e70488e753e3ad96ae79
s1ckret <ivan.kovalenko@s1ckret.com>
2024-01-22 09:26:26 +0200

    feat: add input on book name
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ function Header() {
   return (
     <header className="flex w-full justify-between content-between items-center py-2 px-6 border-b-2 border-black">
       <div>
-        <h1 className="text-2xl">Book title</h1>
+        <input className="text-2xl px-2 rounded-md border-2 border-transparent 
+        hover:border-black focus:border-blue-700 focus:outline-none"
+         spellCheck="false" autoComplete="false" type="text" defaultValue="Untitled book"/>
       </div>
       <div>
         <BsPersonFill size={"2rem"} />

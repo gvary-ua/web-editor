08aef34f9d297712382a296633d445ff566f890f
s1ckret <ivan.kovalenko@s1ckret.com>
2024-01-22 09:55:48 +0200

    feat: show hide nav
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import NavChapter from './NavChapter';
-import { BsChevronDoubleLeft } from "react-icons/bs";
+import { BsChevronDoubleLeft, BsChevronDoubleRight } from "react-icons/bs";
 
 function Nav() {
+  const [showNav, setShowNav] = useState(true);
+
+  const toggleNav = () => {
+    setShowNav(!showNav);
+  };
+
   return (
-    <nav className="w-60 py-2 px-6 border-r-2 border-r-black">
-      <div className='flex justify-between items-center content-center pb-4'>
-        <h1 className="flex-col text-2xl">Chapters</h1>
-        <BsChevronDoubleLeft className="flex-col" size={"1rem"} />
+    <div className="h-full relative">
+      <div onClick={toggleNav} className='absolute top-4 -right-6 p-1 border-2 border-l-0 border-black cursor-pointer z-50 block'>
+        {showNav ? (
+          <BsChevronDoubleLeft size={"1rem"} />
+        ) : (
+          <BsChevronDoubleRight size={"1rem"} />
+        )}
       </div>
-      <NavChapter />
-      <NavChapter />
-      <NavChapter />
-    </nav>
+      <nav className={`flex-shrink-0 h-full w-60 py-2 px-6 border-r-2 border-r-black ${showNav ? '' : 'hidden'}`}>
+        <div className='flex justify-between items-center content-center pb-4'>
+          <h1 className="text-xl">Chapters</h1>
+        </div>
+        <NavChapter />
+        <NavChapter />
+        <NavChapter />
+      </nav>
+    </div>
   );
 };
 

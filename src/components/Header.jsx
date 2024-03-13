aca26ac1f5b77992f402091bcc2ced9ae51df287
k-a-webdev <kuzko.andrii.webdev@gmail.com>
2024-03-13 16:12:39 +0200

    build(#19): add Header component
    Signed-off-by: k-a-webdev <kuzko.andrii.webdev@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,38 @@
+import { useEffect, useState } from 'react';
 import { BsPersonFill } from 'react-icons/bs';
 
-function Header() {
+import { Logo } from './Logo';
+
+export default function Header() {
+  const [name, setName] = useState('');
+  const [profileImg, setProfileImg] = useState('');
+
+  const requestData = () => {
+    setName('Lidia Moroz');
+    setProfileImg('./img/profile_1.png');
+  };
+
+  useEffect(() => {
+    requestData();
+  }, []);
+
   return (
-    <header className="flex w-full justify-between content-between items-center py-2 px-6 border-b-2 border-black">
-      <div>
-        <input className="text-2xl px-2 rounded-md border-2 border-transparent 
-        hover:border-black focus:border-blue-700 focus:outline-none"
-         spellCheck="false" autoComplete="false" type="text" defaultValue="Untitled book"/>
+    <header className="flex h-14 w-full items-center justify-between bg-secondary-1 p-4">
+      <div className="flex h-full cursor-pointer items-center">
+        <Logo />
       </div>
       <div>
-        <BsPersonFill size={"2rem"} />
+        <img
+          src="./icons/burger_menu.svg"
+          alt="Burger menu"
+          className="h-full cursor-pointer sm:hidden"
+        />
+
+        <div className="hidden cursor-pointer flex-row items-center sm:flex">
+          <BsPersonFill className="mr-1 h-6 w-6" size={'2rem'} />
+          <span className="font-robotoFlex text-sm font-medium leading-4">{name}</span>
+        </div>
       </div>
     </header>
   );
 }
-
-export default Header;

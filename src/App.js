7735f21e29cb8bf6d6b859396edb019ea8146400
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-15 09:41:06 +0200

    fix: remove temporary editorJS
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
-import React, { useState } from 'react';
-import Editor from './Editor';
-import Nav from './components/Nav';
+import React from 'react';
 import Header from './components/Header';
 
-// Initial Data
-const INITIAL_DATA = {
-  time: new Date().getTime(),
-  blocks: [
-    {
-      type: 'header',
-      data: {
-        text: 'This is my awesome editor!',
-        level: 1,
-      },
-    },
-  ],
-};
-
 function App() {
-  const [data, setData] = useState(INITIAL_DATA);
   return (
-    <div className="flex h-full flex-col">
+    <React.Fragment>
       <Header />
-      <div className="flex h-full w-full overflow-y-hidden">
-        <Nav className="h-full" />
-        <main className="w-full overflow-y-scroll py-4">
-          <Editor data={data} onChange={setData} editorblock="editorjs-container" />
-        </main>
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
 

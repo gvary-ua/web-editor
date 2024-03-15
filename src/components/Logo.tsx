bef788210e90159ec2ae862cb808e10e211862c9
s1ckret <ivan.kovalenko@s1ckret.com>
2024-03-15 09:38:29 +0200

    fix: simplify Logo component
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,38 +1,12 @@
-import React, { Fragment, HTMLAttributes, forwardRef } from 'react';
-import { FC } from 'react';
-import { VariantProps, cva } from 'class-variance-authority';
-import { twMerge } from 'tailwind-merge';
-import logoSVG from '../img/Logo.svg';
+import React, { HTMLAttributes } from 'react';
 
-const LogoVariants = cva('font-robotoFlex font-bold text-[1.4rem] font-normal leading-[1rem] align-middle', {
-  variants: {
-    text: {
-      true: 'inline',
-      false: 'hidden',
-    },
-  },
-  defaultVariants: {
-    text: true,
-  },
-});
-
-interface LogoProps extends HTMLAttributes<HTMLDivElement>, VariantProps<typeof LogoVariants> {}
-
-const Logo: FC<LogoProps> = forwardRef<HTMLDivElement, LogoProps>(
-  ({ className, text, ...props }, ref) => {
-    return (
-        <Fragment>
-            <img className='inline w-[3rem] h-[2rem]' src={logoSVG} alt='Logo svg'></img>
-            <span
-                {...props}
-                className={twMerge(
-                    LogoVariants({text}),
-                    className
-                )}
-            >Gvary</span>
-        </Fragment>
-    )
-  },
-);
-
-export { Logo, LogoVariants };
+export default function Logo({
+  withText,
+  ...props
+}: HTMLAttributes<HTMLImageElement> & { withText?: boolean }) {
+  return withText ? (
+    <img src="/icons/Logo_text.svg" alt="Logo" {...props} />
+  ) : (
+    <img src="/icons/Logo.svg" alt="Logo" {...props} />
+  );
+}

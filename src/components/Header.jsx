cedd2a78060e12c74e0034c0a5f8c13544cbd24e
G2p1 <andrii.tiuitunnyk@gmail.com>
2024-04-02 00:41:26 +0300

    feat:add children for putt in title name
    Signed-off-by: G2p1 <andrii.tiuitunnyk@gmail.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 
 import Logo from './Logo';
 
-export default function Header() {
+export default function Header({ children }) {
   const [name, setName] = useState('username');
   const [profileImg, setProfileImg] = useState('/icons/user.svg');
 
@@ -18,7 +18,7 @@ export default function Header() {
   return (
     <header className="min flex h-14 w-full items-center justify-between bg-secondary-1 px-4 py-4 md:h-[4.25rem] md:px-20">
       <Logo withText class="h-full cursor-pointer" />
-
+      {children}
       <div>
         <img
           src="/icons/burger_menu.svg"
@@ -27,10 +27,7 @@ export default function Header() {
         />
 
         <div className="hidden cursor-pointer flex-row items-center md:flex">
-          <img
-            src={profileImg}
-            alt="User"
-            className="h-full w-6 cursor-pointer pr-1"/>
+          <img src={profileImg} alt="User" className="h-full w-6 cursor-pointer pr-1" />
           <span className="font-robotoFlex text-sm font-medium leading-4">{name}</span>
         </div>
       </div>

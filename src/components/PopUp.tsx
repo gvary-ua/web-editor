1b32944e8fd0767eaad2b1b753c6a71876ce0121
s1ckret <ivan.kovalenko@s1ckret.com>
2024-06-18 18:20:36 +0300

    fix(#25): cleanup popup
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -1,34 +1,30 @@
-import React, { ReactNode, useState } from 'react';
+import React, { ReactNode } from 'react';
 import { P } from './P';
 
 type IPopUp = React.FC<{
   label: string;
   show: boolean;
+  setShow: (boolean) => {};
   children: ReactNode[];
 }>;
 
-const PopUp: IPopUp = ({ label, show, children }) => {
-  const [isShow, setIsShow] = useState(show);
-
+const PopUp: IPopUp = ({ label, show, setShow, children }) => {
   return (
-    isShow && (
+    show && (
       <div
         className="absolute left-0 top-0 flex h-screen w-full items-center justify-center"
         style={{ backgroundColor: 'rgba(35, 31, 32, 0.25)' }}
       >
-        <div className="sm:items-left sm:w-[37rem] relative flex h-full w-full flex-col items-center justify-center bg-background p-8 sm:h-min sm:items-start sm:rounded-lg">
-          <div className="absolute right-8 top-8 flex h-8 w-8 items-center justify-center">
-            <img
-              src="./icons/Close.svg"
-              alt="Close icon"
-              className="w-4 cursor-pointer"
-              onClick={() => setIsShow(false)}
-            />
-          </div>
-
+        <div className="sm:items-left relative z-50 h-full w-full  bg-background p-8 sm:h-min sm:w-fit sm:items-start sm:rounded-lg">
           <P size="2xl" weight="med" className="text-center sm:text-left">
             {label}
           </P>
+          <img
+            src="./icons/Close.svg"
+            alt="Close icon"
+            className="absolute right-4 top-4 w-4 cursor-pointer"
+            onClick={() => setShow(false)}
+          />
           {children}
         </div>
       </div>

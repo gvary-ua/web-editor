48e44da9c7882fbcf530b1cde9ae2a3f17bd598e
s1ckret <ivan.kovalenko@s1ckret.com>
2024-06-19 23:01:04 +0300

    feat(#29): add onBurgerClick to header
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react';
 
 import Logo from './Logo';
 
-export default function Header({ user, children }) {
+export default function Header({ user, children, onBurgerClick }) {
   const [profileImg, setProfileImg] = useState('/icons/user.svg');
 
   return (
-    <header className="min flex h-14 w-full items-center justify-between bg-secondary-1 px-4 py-4 md:h-[4.25rem] md:px-20">
+    <header className="flex h-14 w-full items-center justify-between bg-secondary-1 px-4 py-4 md:h-[4.25rem] md:px-20">
       {window.innerWidth > 767 && <Logo withText className="h-full cursor-pointer" />}
       {window.innerWidth < 768 && <Logo className="h-full cursor-pointer" />}
       {children}
@@ -15,6 +15,7 @@ export default function Header({ user, children }) {
           src="/icons/burger_menu.svg"
           alt="Burger menu"
           className="h-full cursor-pointer md:hidden"
+          onClick={onBurgerClick}
         />
 
         <div className="hidden cursor-pointer flex-row items-center md:flex">

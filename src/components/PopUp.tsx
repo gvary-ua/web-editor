7422404790e58427276c8f611893b0bb8e1270b0
s1ckret <ivan.kovalenko@s1ckret.com>
2024-06-24 16:17:54 +0300

    fix(#31): fix PopUp bottom padding
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -13,7 +13,7 @@ const PopUp: IPopUp = ({ label, show, setShow, children }) => {
     show && (
       <React.Fragment>
         <div className="absolute left-0 top-0 z-40 hidden min-h-full min-w-full bg-[rgba(35,31,32,0.25)] md:block"></div>
-        <div className="absolute left-0 top-0 z-50 h-full w-full bg-background px-5 md:left-1/2 md:top-1/2 md:h-min md:w-fit md:-translate-x-1/2 md:-translate-y-3/4 md:rounded-lg md:px-8">
+        <div className="absolute left-0 top-0 z-50 h-full w-full bg-background px-5 pb-5 md:left-1/2 md:top-1/2 md:h-min md:w-fit md:-translate-x-1/2 md:-translate-y-3/4 md:rounded-lg md:px-8">
           <div className="flex w-full items-center justify-between py-5">
             <P size="2xl" weight="med" className="text-center sm:text-left">
               {label}

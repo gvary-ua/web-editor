9bc58def7a53a0bc80f918cdf939ce5995346011
s1ckret <ivan.kovalenko@s1ckret.com>
2024-06-28 16:34:06 +0300

    feat(#35): load blocks
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import Header from './components/Header';
 import Editor from './components/Editor';
 import { Button } from './components/Button';
@@ -6,38 +6,44 @@ import PopUp from './components/PopUp';
 import SideMenu from './components/SideMenu';
 import useCurrentUser from './apis/useUser';
 import Chapters from './components/Chapters';
-
-const INITIAL_DATA = {
-  time: new Date().getTime(),
-  blocks: [
-    {
-      type: 'header',
-      data: {
-        text: 'This is my awesome editor!',
-        level: 1,
-      },
-    },
-  ],
-};
+import { GlobalContext } from './GlobalContext';
+import { P } from './components/P';
+import { useBlocksByChapterId } from './apis/useBlocks';
 
 function App() {
   const urlParams = new URLSearchParams(window.location.search);
   const coverId = parseInt(urlParams.get('coverId'));
 
-  const [data, setData] = React.useState(INITIAL_DATA);
-  const [mobileNavOpen, setMobileNavOpen] = React.useState(false);
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+  const { activeChapter } = useContext(GlobalContext);
+  const chapterId = activeChapter.get?.id;
 
-  const { data: user, isError, isLoading } = useCurrentUser();
+  const {
+    data: blocks,
+    isError: isBlockError,
+    isLoading: isBlockLoading,
+  } = useBlocksByChapterId(chapterId, { enabled: !!chapterId });
+  const { data: user, isError: isUserError, isLoading: isUserLoading } = useCurrentUser();
 
   const chapters = <Chapters coverId={coverId} />;
 
-  if (isLoading) {
+  if (isUserLoading | isBlockLoading) {
     return 'Loading...';
   }
 
-  if (isError) {
+  if (isBlockError) {
     return (
-      <PopUp label={'Please login!'} show={isError}>
+      <PopUp label={'Error loading blocks!'} show={isBlockError}>
+        <div className="min-w-60"></div>
+        <P>Please try reloading the page.</P>
+        <Button onClick="window.location.reload();">Reload Page</Button>
+      </PopUp>
+    );
+  }
+
+  if (isUserError) {
+    return (
+      <PopUp label={'Please login!'} show={isUserError}>
         {/* TODO: Put it in config */}
         {/* TODO: After I press login redirect me back to SPA */}
         <div className="min-w-60"></div>
@@ -58,7 +64,11 @@ function App() {
       <div className="flex h-auto max-h-[calc(100vh-3.5rem)] min-h-[calc(100vh-3.5rem)] w-full md:max-h-[calc(100vh-4.25rem)] md:min-h-[calc(100vh-4.25rem)] md:pl-20 ">
         <SideMenu className="hidden overflow-y-auto  pt-10 md:block">{chapters}</SideMenu>
         <main className="w-full overflow-y-auto px-3 py-3 md:px-20 md:pt-10">
-          <Editor data={data} onChange={setData} editorblock="editorjs-container" />
+          <Editor
+            data={blocks}
+            onChange={(data) => console.log(data.blocks)}
+            editorblock="editorjs-container"
+          />
         </main>
       </div>
     </React.Fragment>

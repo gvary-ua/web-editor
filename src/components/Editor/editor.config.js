e85f20da5e088c64a542964896411738bb9629e7
s1ckret <ivan.kovalenko@s1ckret.com>
2024-06-28 15:54:44 +0300

    feat(#33): add VersionBlockTune
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/components/Editor/editor.config.js
+++ src/components/Editor/editor.config.js
@@ -6,6 +6,7 @@ import CheckList from '@editorjs/checklist';
 
 import Strikethrough from '@sotaproject/strikethrough';
 import Underline from '@editorjs/underline';
+import VersionBlockTune from './VersionBlockTune';
 
 export const EDITOR_JS_TOOLS = {
   strikethrough: Strikethrough,
@@ -35,4 +36,19 @@ export const EDITOR_JS_TOOLS = {
     inlineToolbar: true,
   },
   delimiter: Delimiter,
+  versionTune: {
+    class: VersionBlockTune,
+    // Here you must list all blocks that we are using and their version.
+    // Follow semantic versioning.
+    // If you add, remove or change data content of a block, then you must bump the version.
+    config: {
+      blockVersions: {
+        header: '1.0.0',
+        paragraph: '1.0.0',
+        list: '1.0.0',
+        checkList: '1.0.0',
+        delimiter: '1.0.0',
+      },
+    },
+  },
 };

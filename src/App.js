90a70abcc966c6a83394259de79412d776dde2cb
s1ckret <ivan.kovalenko@s1ckret.com>
2024-06-28 13:46:50 +0300

    fix(#35): fix padding and remove unnecessary tools
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
       </PopUp>
       <div className="flex h-auto max-h-[calc(100vh-3.5rem)] min-h-[calc(100vh-3.5rem)] w-full md:max-h-[calc(100vh-4.25rem)] md:min-h-[calc(100vh-4.25rem)] md:pl-20 ">
         <SideMenu className="hidden overflow-y-auto  pt-10 md:block">{chapters}</SideMenu>
-        <main className="w-full overflow-y-auto px-3 py-3 md:pl-9 md:pt-10">
+        <main className="w-full overflow-y-auto px-3 py-3 md:px-20 md:pt-10">
           <Editor data={data} onChange={setData} editorblock="editorjs-container" />
         </main>
       </div>
--- src/components/Editor/editor.config.js
+++ src/components/Editor/editor.config.js
@@ -1,37 +1,15 @@
-// Blocks
 import Paragraph from '@editorjs/paragraph';
 import Header from '@editorjs/header';
 import List from '@editorjs/list';
-import Link from '@editorjs/link';
 import Delimiter from '@editorjs/delimiter';
 import CheckList from '@editorjs/checklist';
-import Table from '@editorjs/table';
 
-//Tools
 import Strikethrough from '@sotaproject/strikethrough';
 import Underline from '@editorjs/underline';
 
 export const EDITOR_JS_TOOLS = {
   strikethrough: Strikethrough,
   underline: Underline,
-  paragraph: {
-    class: Paragraph,
-    inlineToolbar: true,
-    config: {
-      placeholder: 'Enter a text....',
-    },
-  },
-  //
-  checkList: {
-    class: CheckList,
-    inlineToolbar: true,
-  },
-  //
-  list: {
-    class: List,
-    inlineToolbar: true,
-  },
-  //
   header: {
     class: Header,
     inlineToolbar: true,
@@ -41,21 +19,20 @@ export const EDITOR_JS_TOOLS = {
       defaultLevel: 2,
     },
   },
-  //
-  delimiter: Delimiter,
-  //
-  link: {
-    class: Link,
-    inlineToolbar: true,
-  },
-  //
-  table: {
-    class: Table,
+  paragraph: {
+    class: Paragraph,
     inlineToolbar: true,
     config: {
-      withHeadings: true, // I don't know why it doesn't work.
-      rows: 2,
-      cols: 3,
+      placeholder: 'Enter a text....',
     },
   },
+  list: {
+    class: List,
+    inlineToolbar: true,
+  },
+  checkList: {
+    class: CheckList,
+    inlineToolbar: true,
+  },
+  delimiter: Delimiter,
 };

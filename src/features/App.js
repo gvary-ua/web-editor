e8819390e254892420c661f8b22c91f742792da8
s1ckret <ivan.kovalenko@s1ckret.com>
2024-06-29 10:45:38 +0300

    refactor(#35): move mapping to separate file
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/features/App.js
+++ src/features/App.js
@@ -10,6 +10,7 @@ import { GlobalContext } from 'context/GlobalContext';
 import { P } from 'components/P';
 import { useBlocksByChapterId, useBlocksCreate } from 'apis/useBlocks';
 import { useChaptersByCoverId } from 'apis/useChapters';
+import { dbBlockToEditorBlock, editorBlockToDbBlock } from 'functions/mapper';
 
 function App() {
   const urlParams = new URLSearchParams(window.location.search);
@@ -119,55 +120,4 @@ function App() {
   );
 }
 
-function editorBlockTypeToId(type) {
-  switch (type) {
-    case 'header':
-      return 1;
-    case 'paragraph':
-      return 2;
-    case 'list':
-      return 3;
-    case 'checkList':
-      return 4;
-    case 'delimiter':
-      return 5;
-    default:
-      throw new Error('This type is not supported!', type);
-  }
-}
-
-function dbBlockTypeIdToString(type) {
-  switch (type) {
-    case 1:
-      return 'header';
-    case 2:
-      return 'paragraph';
-    case 3:
-      return 'list';
-    case 4:
-      return 'checkList';
-    case 5:
-      return 'delimiter';
-    default:
-      throw new Error('This id is not supported!', type);
-  }
-}
-
-function editorBlockToDbBlock(editorBlock) {
-  return {
-    id: editorBlock.id,
-    typeId: editorBlockTypeToId(editorBlock.type),
-    data: editorBlock.data,
-    dataVersion: editorBlock.tunes.versionTune.version,
-    wordCount: 0,
-  };
-}
-
-function dbBlockToEditorBlock(dbBlock) {
-  return {
-    id: dbBlock.id,
-    data: dbBlock.data,
-    type: dbBlockTypeIdToString(dbBlock.typeId),
-  };
-}
 export default App;

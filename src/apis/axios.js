2ecf26aafb764b12dd2eea9e9e432ee405334c6c
s1ckret <ivan.kovalenko@s1ckret.com>
2024-08-06 10:23:09 +0300

    build(#61): add support for multiple envs
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/apis/axios.js
+++ src/apis/axios.js
@@ -2,7 +2,7 @@ import Cookies from 'js-cookie';
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   withCredentials: true,
   withXSRFToken: true,
   xsrfCookieName: 'XSRF-TOKEN',

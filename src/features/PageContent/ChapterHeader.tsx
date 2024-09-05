16cf40ef86a9e9029a70bb2747c878248db28502
s1ckret <ivan.kovalenko@s1ckret.com>
2024-09-05 11:06:39 +0300

    fix(#66): fix padding margin in cover header
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/features/PageContent/ChapterHeader.tsx
+++ src/features/PageContent/ChapterHeader.tsx
@@ -13,7 +13,7 @@ export default function ChapterHeader() {
       type="text"
       placeholder="Назва глави"
       value={activeChapter.get?.title || ''}
-      className="mb-[3px] mt-[0.6em] w-full rounded-md py-2 font-robotoFlex text-[2rem] leading-[2rem] hover:bg-surface-2"
+      className="mb-[3px] mt-[0.6em] w-full rounded-md font-robotoFlex text-[2rem] leading-[2rem] hover:bg-surface-2"
       onChange={(e) => activeChapter?.set({ ...activeChapter.get, title: e.target.value })}
       onBlur={(e) => updateTitle({ id: activeChapter.get?.id, title: e.target.value })}
     ></input>
--- src/features/PageContent/CoverHeader.tsx
+++ src/features/PageContent/CoverHeader.tsx
@@ -14,7 +14,7 @@ export default function CoverHeader() {
       type="text"
       placeholder="Назва книги"
       value={title}
-      className="mb-[3px] mt-[0.6em] w-full rounded-md py-2 font-roslindaleCyrillic text-[3rem] leading-[3rem] hover:bg-surface-2"
+      className="mb-[3px] mt-[0.6em] w-full rounded-md font-roslindaleCyrillic text-[2.5rem] leading-[1.2] hover:bg-surface-2"
       onChange={(e) => setTitle(e.target.value)}
       onBlur={(e) => updateCoverTitle({ coverId: cover.id, title: e.target.value })}
     ></input>

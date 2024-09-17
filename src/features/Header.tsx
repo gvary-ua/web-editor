09ae96866a26fd671b73e58eb0cf7a80e220da7e
s1ckret <ivan.kovalenko@s1ckret.com>
2024-09-17 14:27:44 +0300

    feat(#74): add publish button to a header
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -1,24 +1,39 @@
 import { User } from 'apis/types/user';
+import { Button } from 'components/Button';
 import Logo from 'components/Logo';
-import React from 'react';
+import { GlobalContext } from 'context/GlobalContext';
+import React, { useContext } from 'react';
 
 export default function Header({ user, onBurgerClick }: { user: User; onBurgerClick: () => void }) {
+  const { coverId } = useContext(GlobalContext);
+
   return (
     <header className="flex h-14 w-full items-center justify-between bg-secondary-1 px-4 py-4 sm:px-10 md:h-[4.25rem] md:px-14 lg:px-16">
       <a className="h-full" href={process.env.REACT_APP_API_BASE_URL}>
         <Logo withText className="h-full" />
       </a>
       <div>
-        <img
-          src="/icons/burger_menu.svg"
-          alt="Burger menu"
-          className="h-full cursor-pointer md:hidden"
-          onClick={onBurgerClick}
-        />
-
-        <div className="hidden cursor-pointer flex-row items-center px-4 py-2 md:flex">
-          <img src="/icons/user.svg" alt="User" className="h-full w-6 pr-1" />
-          <span className="font-robotoFlex text-sm font-medium leading-4">{user.login}</span>
+        <div className="flex flex-row items-center">
+          <Button
+            size="xs"
+            variant="primary"
+            className="mr-4"
+            href={process.env.REACT_APP_API_BASE_URL + '/books/' + coverId + '/edit'}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Опублікувати
+          </Button>
+          <img
+            src="/icons/burger_menu.svg"
+            alt="Burger menu"
+            className="h-full cursor-pointer md:hidden"
+            onClick={onBurgerClick}
+          />
+          <div className="hidden cursor-pointer flex-row items-center px-4 py-2 md:flex">
+            <img src="/icons/user.svg" alt="User" className="h-full w-6 pr-1" />
+            <span className="font-robotoFlex text-sm font-medium leading-4">{user.login}</span>
+          </div>
         </div>
       </div>
     </header>

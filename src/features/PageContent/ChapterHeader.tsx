2337ce328c93b60b32377a998caf9a0668b69872
s1ckret <ivan.kovalenko@s1ckret.com>
2024-09-18 13:48:15 +0300

    feat(#77): forbid to set empty titles
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/features/PageContent/ChapterHeader.tsx
+++ src/features/PageContent/ChapterHeader.tsx
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { GlobalContext } from 'context/GlobalContext';
 import { useChapterPartialUpdate } from 'apis/useChapters';
 
 export default function ChapterHeader() {
   const { activeChapter } = useContext(GlobalContext);
   const { mutate: updateTitle } = useChapterPartialUpdate();
+  const [titleOnFocus, setTitleOnFocus] = useState(activeChapter.get?.title);
 
   return (
     <input
@@ -15,7 +16,15 @@ export default function ChapterHeader() {
       value={activeChapter.get?.title || ''}
       className="mb-[3px] mt-[0.6em] w-full rounded-md font-robotoFlex text-[2rem] leading-[2rem] hover:bg-surface-2"
       onChange={(e) => activeChapter?.set({ ...activeChapter.get, title: e.target.value })}
-      onBlur={(e) => updateTitle({ id: activeChapter.get?.id, title: e.target.value })}
+      onFocus={(e) => setTitleOnFocus(e.target.value)}
+      onBlur={(e) => {
+        const s = e.target.value;
+        if (s && s !== '' && s !== activeChapter.get?.title) {
+          updateTitle({ id: activeChapter.get?.id, title: s });
+        } else {
+          activeChapter?.set({ ...activeChapter.get, title: titleOnFocus });
+        }
+      }}
     ></input>
   );
 }
--- src/features/PageContent/CoverHeader.tsx
+++ src/features/PageContent/CoverHeader.tsx
@@ -5,6 +5,7 @@ import { useCoverUpdate } from 'apis/useCover';
 export default function CoverHeader() {
   const { cover } = useContext(GlobalContext);
   const [title, setTitle] = useState(cover.title);
+  const [titleOnFocus, setTitleOnFocus] = useState(cover.title);
   const { mutate: updateCoverTitle } = useCoverUpdate();
 
   return (
@@ -16,7 +17,15 @@ export default function CoverHeader() {
       value={title}
       className="mb-[3px] mt-[0.6em] w-full rounded-md font-roslindaleCyrillic text-[2.5rem] leading-[1.2] hover:bg-surface-2"
       onChange={(e) => setTitle(e.target.value)}
-      onBlur={(e) => updateCoverTitle({ coverId: cover.id, title: e.target.value })}
+      onFocus={(e) => setTitleOnFocus(e.target.value)}
+      onBlur={(e) => {
+        const s = e.target.value;
+        if (s && s !== '' && s !== title) {
+          updateCoverTitle({ coverId: cover.id, title: s });
+        } else {
+          setTitle(titleOnFocus);
+        }
+      }}
     ></input>
   );
 }

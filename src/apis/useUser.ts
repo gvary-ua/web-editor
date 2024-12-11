2843e76c39db3ff2988cd10a60b45460ede7a8e6
s1ckret <ivan.kovalenko@s1ckret.com>
2024-12-11 13:36:27 +0200

    feat(#83): set user img and display name
    Signed-off-by: s1ckret <ivan.kovalenko@s1ckret.com>

--- src/apis/useUser.ts
+++ src/apis/useUser.ts
@@ -1,13 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import api from 'apis/axios';
-import { User } from './types/user';
+import { User, UserResponse } from './types/user';
 
 export default function useCurrentUser() {
   return useQuery<User, Error>({
     queryKey: ['current-user'],
     queryFn: async () => {
-      const response = await api.get<User>('/api/user');
-      return response.data;
+      const response = await api.get<UserResponse>('/api/user');
+      return response.data.data;
     },
   });
 }
--- src/features/Header.tsx
+++ src/features/Header.tsx
@@ -6,7 +6,6 @@ import React, { useContext } from 'react';
 
 export default function Header({ user, onBurgerClick }: { user: User; onBurgerClick: () => void }) {
   const { coverId } = useContext(GlobalContext);
-
   return (
     <header className="flex h-14 w-full items-center justify-between bg-secondary-1 px-4 py-4 sm:px-10 md:h-[4.25rem] md:px-14 lg:px-16">
       <a className="h-full" href={process.env.REACT_APP_API_BASE_URL}>
@@ -30,10 +29,14 @@ export default function Header({ user, onBurgerClick }: { user: User; onBurgerCl
             className="h-full cursor-pointer md:hidden"
             onClick={onBurgerClick}
           />
-          <div className="hidden cursor-pointer flex-row items-center px-4 py-2 md:flex">
-            <img src="/icons/user.svg" alt="User" className="h-full w-6 pr-1" />
-            <span className="font-robotoFlex text-sm font-medium leading-4">{user.login}</span>
-          </div>
+          <a href={process.env.REACT_APP_API_BASE_URL + '/profile/' + user.id}>
+            <div className="hidden flex-row items-center px-4 py-2 md:flex">
+              <img src={user.imgUrl} alt="User" className="mr-1 h-full w-6 rounded-full" />
+              <span className="font-robotoFlex text-sm font-medium leading-4">
+                {user.displayName}
+              </span>
+            </div>
+          </a>
         </div>
       </div>
     </header>
